8a060c1d0cd72f1e6b2e12a3ee64c20ad1d1fa39
S7ns3t <newcompletelyemptymail@mail.ru>
2024-12-20 15:17:20 +0300

    Добавьте файлы проекта.

---
+++ EdP_Alimov/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace EdP_Alimov
+{
+
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int UserPositionID = ((App)Application.Current).CurrentUserPositionID;
+
+            if (UserPositionID == 1)
+            {
+                Employees.Visibility = Visibility.Collapsed;
+                Reports.Visibility = Visibility.Collapsed;
+                Equipment.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Feedback.Visibility = Visibility.Collapsed;
+            }
+
+            MainFrame = MainFrame;
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (!(e.Content is Page page)) return;
+            this.Title = $"EdP_Alimov - {page.Title}";
+        }
+
+        private void Employees_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("Pages/Employees.xaml", UriKind.Relative));
+        }
+
+        private void Clients_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("Pages/Clients.xaml", UriKind.Relative));
+        }
+
+        private void Equipment_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("Pages/Equipments.xaml", UriKind.Relative));
+        }
+
+        private void Requests_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("Pages/Requests.xaml", UriKind.Relative));
+        }
+
+        private void Logs_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("Pages/RequestLogs.xaml", UriKind.Relative));
+        }
+
+        private void Reports_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("Pages/Reports.xaml", UriKind.Relative));
+        }
+
+        private void Feedback_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("Pages/QRPage.xaml", UriKind.Relative));
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var result = MessageBox.Show("Вы действительно хотите выйти?", "Выход", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ EdP_Alimov/Pages/RequestsAddPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EdP_Alimov.Pages
+{
+    public partial class RequestAddEdit : Page
+    {
+        int saveid, what;
+
+        public RequestAddEdit(Requests selectedRequest)
+        {
+            InitializeComponent();
+
+            ComboBoxEquipment.ItemsSource = Entities.GetContext().Equipment.ToList();
+            ComboBoxFaultType.ItemsSource = Entities.GetContext().FaultTypes.ToList();
+            ComboBoxClient.ItemsSource = Entities.GetContext().Clients.ToList();
+            ComboBoxStatus.ItemsSource = Entities.GetContext().Statuses.ToList();
+            ComboBoxExecutor.ItemsSource = Entities.GetContext().Executors.ToList();
+            if (selectedRequest != null)
+            {
+                using (var db = new Entities())
+                {
+                    saveid = selectedRequest.RequestID;
+                    TextBoxRequestNumber.Text = selectedRequest.RequestNumber;
+                    DatePickerDateAdded.SelectedDate = selectedRequest.DateAdded;
+                    ComboBoxEquipment.SelectedValue = selectedRequest.EquipmentID;
+                    ComboBoxFaultType.SelectedValue = selectedRequest.FaultTypeID;
+                    TextBoxProblemDescription.Text = selectedRequest.ProblemDescription;
+                    ComboBoxClient.SelectedValue = selectedRequest.ClientID;
+                    ComboBoxStatus.SelectedValue = selectedRequest.StatusID;
+                    ComboBoxExecutor.SelectedValue = selectedRequest.ExecutorID;
+                    ComboBoxPriority.Text = selectedRequest.Priority;
+                    DatePickerCompletionDate.SelectedDate = selectedRequest.CompletionDate;
+                    what = 1;
+                }
+
+            }
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new Entities())
+                {
+                    if (what == 0)
+                    {
+
+                        {
+                            Requests words = new Requests();
+                            words.RequestNumber = TextBoxRequestNumber.Text;
+                            words.DateAdded = DatePickerDateAdded.SelectedDate ?? DateTime.Now;
+                            words.EquipmentID = (int)ComboBoxEquipment.SelectedValue;
+                            words.FaultTypeID = (int)ComboBoxFaultType.SelectedValue;
+                            words.ProblemDescription = TextBoxProblemDescription.Text;
+                            words.ClientID = (int)ComboBoxClient.SelectedValue;
+                            words.StatusID = (int)ComboBoxStatus.SelectedValue;
+                            words.ExecutorID = (int)ComboBoxExecutor.SelectedValue;
+                            words.Priority = ComboBoxPriority.Text;
+                            words.CompletionDate = DatePickerCompletionDate.SelectedDate;
+                            context.Requests.Add(words);
+                            context.SaveChanges();
+                        };
+                    }
+                    else
+                    {
+                        var existingPost = context.Requests.FirstOrDefault(p => p.RequestID == saveid);
+                        existingPost.RequestNumber = TextBoxRequestNumber.Text;
+                        existingPost.DateAdded = DatePickerDateAdded.SelectedDate ?? DateTime.Now;
+                        existingPost.EquipmentID = (int)ComboBoxEquipment.SelectedValue;
+                        existingPost.FaultTypeID = (int)ComboBoxFaultType.SelectedValue;
+                        existingPost.ProblemDescription = TextBoxProblemDescription.Text;
+                        existingPost.ClientID = (int)ComboBoxClient.SelectedValue;
+                        existingPost.StatusID = (int)ComboBoxStatus.SelectedValue;
+                        existingPost.ExecutorID = (int)ComboBoxExecutor.SelectedValue;
+                        existingPost.Priority = ComboBoxPriority.Text;
+                        existingPost.CompletionDate = DatePickerCompletionDate.SelectedDate;
+                        context.Entry(existingPost).State = EntityState.Modified;
+                        context.SaveChanges();
+                    }
+                    MessageBox.Show("Данные успешно сохранены!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                    NavigationService.GoBack();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка сохранения: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
